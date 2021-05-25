7af75936b9fdb8da3ea7ead0ea18828ad12482d0
Lucas Coady <lcoady@Cyclone.local>
2021-05-25 12:03:15 -0500

    refactored

--- repquota.py
+++ repquota.py
@@ -9,44 +9,7 @@ from time import ctime
 db_file = '/share/quotas.db'
 
 
-def get_username():
-    return pwd.getpwuid(os.getuid())[0]
-
-
-def dict_factory(cursor, row):
-	d = {}
-	for idx, col in enumerate(cursor.description):
-		if type(row[idx]) == str and row[idx] == 'None':
-			d[col[0]] = None
-		else:
-			d[col[0]] = row[idx]
-	return d
-
-
-def sz(num, suffix='B'):
-	if num is None:
-		return 0
-	for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
-		if abs(num) < 1024.0:
-			return "%3.2f%s%s" % (num, unit, suffix)
-		num /= 1024.0
-	return "%.1f%s%s" % (num, 'Yi', suffix)
-
-
-db_mtime = ctime(os.stat(db_file).st_ctime)
-
-db_connection = sqlite3.connect(db_file)
-
-db = db_connection.cursor()
-
-# Get the list of groups (ints) and then convert to strs
-usergroups = [str(group) for group in os.getgrouplist(get_username(), 0)]
-
-# produce a dictionary instead of a list
-db.row_factory = dict_factory
-db.execute(f"SELECT * from quotas where name='{get_username()}' OR name IN ({','.join(usergroups)}) ORDER BY type DESC, path")
-quotas = db.fetchall()
-names = [column[0] for column in db.description]
+USERNAME = pwd.getpwuid(os.getuid())[0]
 
 
 class QuotaReport():
@@ -54,6 +17,7 @@ class QuotaReport():
 		# The eventual output string to print
 		self.output = pre
 		self.spacing = spacing
+		self.summary = ''
 		self.end = end
 
 	def add_tabular(self, *args, total_line=False, sum_line=False, end='\n'):
@@ -72,76 +36,107 @@ class QuotaReport():
 	def add_line(self, line, end='\n'):
 		self.output += str(line) + end
 
-	def print(self):
-		print(self.output + self.end)
+	def add_summary_line(self, line, end='\n'):
+		self.summary += str(line) + end
 
+	def print(self):
+		print(self.output + self.summary + self.end)
+
+
+class Quota():
+	def __init__(self, quota):
+		if quota['type'] == 'user':
+			self.type = 'user'
+			self.filesystem = None
+			self.fileset = quota['path'].split('/')[3]
+		elif quota['type'] == 'directory':
+			self.type = 'group'
+			self.filesystem = quota['path'].split('/')[3]
+			self.fileset = quota['path'].split('/')[-1]
+		else:
+			self.type = 'other'
+		self.usage = self.hum_sz(quota['usage'])
+		self.soft = self.hum_sz(quota['soft'])
+		self.hard = self.hum_sz(quota['hard'])
+		self.inodes = quota['inodes']
+		self.percent_free = round(quota['usage'] / quota['soft'] * 100)
+		self.grace_lapsed = False
+		if quota['exceeded'] is not None:
+			if quota['grace'] is not None:
+				grace = timedelta(seconds=int(quota['grace']))
+			else:
+				grace = 0
+			exceeded = datetime.fromtimestamp(int(quota['exceeded']))
+			grace_ending = exceeded + grace
+			grace_left = grace_ending - datetime.now()
+			if grace_left.days > 1:
+				self.grace = str(grace_left.days) + ' days'
+			elif grace_left.seconds < 0:
+				self.grace = 'elaspsed'
+				self.grace_lapsed = True
+			else:
+				self.grace = str(grace_left.hours) + ' hours'
+		else:
+			self.grace = 'none'
+
+	def hum_sz(self, num, suffix='B'):
+		if num is None:
+			return 0
+		for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+			if abs(num) < 1024.0:
+				return "%3.2f%s%s" % (num, unit, suffix)
+			num /= 1024.0
+		return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
+def quota_factory(cursor, row):
+	d = {}
+	for idx, col in enumerate(cursor.description):
+		if type(row[idx]) == str and row[idx] == 'None':
+			d[col[0]] = None
+		else:
+			d[col[0]] = row[idx]
+	return Quota(d)
+
+
+db_mtime = ctime(os.stat(db_file).st_ctime)
+
+db_connection = sqlite3.connect(db_file)
+db = db_connection.cursor()
+db.row_factory = quota_factory
+
+# Get the list of groups (ints) and then convert to strs
+usergroups = [str(group) for group in os.getgrouplist(USERNAME, 0)]
+
+# produce a dictionary instead of a list
+db.execute(f"SELECT * from quotas where name='{USERNAME}' OR name IN ({','.join(usergroups)}) ORDER BY type DESC, path")
+quotas = db.fetchall()
+names = [column[0] for column in db.description]
 
 report = QuotaReport(spacing=[30, 17, 11, 11, 11, 6, 9])
-
-
-warn_grace = False
-warn_soft = False
-warn_hard = False
 
 report.add_tabular(total_line=True)
 report.add_tabular('fileset', 'type', 'used', 'quota', 'limit', '%FULL', 'grace')
 report.add_tabular(sum_line=True)
 
-filesystem = str()
+last_filesystem = ''
+
 for q in quotas:
-	path = q['path'].split('/')[3]
-
-	if q['type'] == 'directory':
-		qtype = 'group'
-		if path != filesystem:
-			filesystem = path
+	if q.type == 'group':
+		# the purpose of this if statement is to create the label for the next group of filesystems
+		# since we know the output from the sqlite3 db is sorted we can rely on this being in order
+		if q.filesystem != last_filesystem:
+			last_filesystem = q.filesystem
 			report.add_tabular(sum_line=True)
-			report.add_line(f'>>> Capacity Filesystem: {filesystem} (IFS)')
+			report.add_line(f'>>> Capacity Filesystem: {q.filesystem} (IFS)')
 			report.add_tabular(sum_line=True)
-		path = q['path'].split('/')[-1]
 
-	elif q['type'] == 'user':
-		qtype = 'user'
-	else:
-		qtype = 'other'
-
-	if q['grace'] is not None:
-		grace = timedelta(seconds=int(q['grace']))
-
-	if q['exceeded'] is not None:
-		exceeded = datetime.fromtimestamp(int(q['exceeded']))
-		grace_ends = exceeded + grace
-		grace_left = grace_ends - datetime.now()
-		if grace_left.days > 1:
-			grace_left_str = str(grace_left.days) + ' days'
-		elif grace_left.seconds < 0:
-			grace_left_str = 'elaspsed'
-			grace_elapsed = True
-		else:
-			grace_left_str = str(grace_left.hours) + ' hours'
-	else:
-		grace_left_str = 'none'
-
-	if q['usage'] is not None:
-		if q['hard'] is not None:
-			if q['hard'] < q['usage']:
-				warn_hard = 'Exceeding your hard limit is preventing you from writing.\n'
-			elif q['usage'] / q['hard'] > .9:
-				warn_hard = 'You have exceeded your soft limit, you must remove files before your grace expires or you will not be able to write.\n'
-				warn_hard += 'You are near your hard limit which may prevent you from writing.'
-			elif q['soft'] is not None and q['usage'] / q['soft'] > .9:
-				warn_soft = 'You are near your soft limit which may prevent you from writing.\n'
-
-	report.add_tabular(path, f"blocks ({qtype})", sz(q['usage']), sz(q['soft']), sz(q['hard']), round(q['usage'] / q['soft'] * 100), grace_left_str)
-	report.add_tabular('', f"files ({qtype})", q['inodes'], 'none', 'none', '', 'none')
+	if q.grace_lapsed:
+		report.add_summary_line(f"The grace period on {q.fileset} has expired. You will not be able to write until you reduce usage.")
+	report.add_tabular(q.fileset, f"blocks ({q.type})", q.usage, q.soft, q.hard, q.percent_free, q.grace)
+	report.add_tabular('', f"files ({q.type})", q.inodes, 'none', 'none', '', 'none')
 
 report.add_tabular(total_line=True)
-
-if warn_hard:
-	report.add_line(warn_hard)
-
-if warn_grace:
-	report.add_line("The grace period on one of the directories you use has elasped.\nThis means that you can no longer write files to this location.\n")
 
 report.print()
 print(f'This information was current as of {db_mtime}.')
