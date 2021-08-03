2900a72d423ce6edb5c90c8250d428a48aeab182
Lucas Coady <lucas@uchicago.edu>
2021-08-03 14:21:02 -0500

    fixed issue with >1000 quotas by adding resume function

--- quotas.py
+++ quotas.py
@@ -15,36 +15,44 @@ CREATE TABLE quotas
 """)
 
 quota_api = QuotaApi(isi_api)
+resume_token = 'FIRST'
 
-try:
-	quotas = quota_api.list_quota_quotas(resolve_names=True).quotas
-except rest.ApiException as err:
-	print(f"Api Exception: {err}")
+while resume_token:
+	try:
+		if resume_token == 'FIRST':
+			quota_query = quota_api.list_quota_quotas(resolve_names=True)
+		elif resume_token:
+			quota_query = quota_api.list_quota_quotas(resume=resume_token)
 
-for quota in quotas:
-	cur = {
-		'type': quota.type,
-		'path': quota.path,
-		'advisory': quota.thresholds.advisory,
-		'hard': quota.thresholds.hard,
-		'soft': quota.thresholds.soft,
-		'exceeded': quota.thresholds.soft_last_exceeded,
-		'grace': quota.thresholds.soft_grace,
-		'inodes': quota.usage.inodes,
-		'usage': quota.usage.to_dict()[quota.thresholds_on[:-4]],
-		'efficiency': quota.efficiency_ratio
-	}
-	if quota.type == "user" and quota.persona.name is not None:
-		cur['name'] = quota.persona.name.split('\\')[-1]
-	elif quota.type == "directory":
-		try:
-			cur['name'] = str(os.stat(quota.path).st_gid)
-		except FileNotFoundError:
-			cur['name'] = str(99)
-	keys = ','.join(cur.keys())
-	values = ','.join('"' + str(x) + '"' for x in cur.values())
-	insert_sql = f"INSERT INTO quotas ({keys}) VALUES ({values});"
-	db.execute(insert_sql)
+		resume_token = quota_query.resume
+
+	except rest.ApiException as err:
+		print(f"Api Exception: {err}")
+
+	for quota in quota_query.quotas:
+		cur = {
+			'type': quota.type,
+			'path': quota.path,
+			'advisory': quota.thresholds.advisory,
+			'hard': quota.thresholds.hard,
+			'soft': quota.thresholds.soft,
+			'exceeded': quota.thresholds.soft_last_exceeded,
+			'grace': quota.thresholds.soft_grace,
+			'inodes': quota.usage.inodes,
+			'usage': quota.usage.to_dict()[quota.thresholds_on[:-4]],
+			'efficiency': quota.efficiency_ratio
+		}
+		if quota.type == "user" and quota.persona.name is not None:
+			cur['name'] = quota.persona.name.split('\\')[-1]
+		elif quota.type == "directory":
+			try:
+				cur['name'] = str(os.stat(quota.path).st_gid)
+			except FileNotFoundError:
+				cur['name'] = str(99)
+		keys = ','.join(cur.keys())
+		values = ','.join('"' + str(x) + '"' for x in cur.values())
+		insert_sql = f"INSERT INTO quotas ({keys}) VALUES ({values});"
+		db.execute(insert_sql)
 
 db_connection.commit()
 db_connection.close()
